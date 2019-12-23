91cca148f0bd253167e82d27b3e0ee268a4181a1
heejink <huijin.kang77@gmail.com>
2019-12-23 16:22:24 +0900

    Update multiplication game

--- js/multiplicationTable.js
+++ js/multiplicationTable.js
@@ -11,9 +11,16 @@ quiz.textContent = String(num1) + " ✖ " + String(num2) + " ? ";
 form.addEventListener("submit", function(eve) {
   eve.preventDefault();
   if (answer.value === String(multiply)) {
+    result.textContent = `딩동댕~🔔`;
+    num1 = Math.ceil(Math.random() * 9);
+    num2 = Math.ceil(Math.random() * 9);
+    multiply = num1 * num2;
+    quiz.textContent = String(num1) + " ✖ " + String(num2) + " ? ";
     answer.value = "";
     answer.focus();
-    result.textContent = `딩동댕~🔔`;
+  } else if (answer.value.length < 1 || answer.value == 0) {
+    alert(`구구단 게임이라구요..💢💥`);
+    answer.focus();
   } else {
     answer.value = "";
     answer.focus();
