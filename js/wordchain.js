928228c685a2e2526273dfa3e7a60fa33f36e3e6
heejink <huijin.kang77@gmail.com>
2019-12-23 14:40:35 +0900

    Change div tag to form tag

--- js/wordchain.js
+++ js/wordchain.js
@@ -2,32 +2,25 @@ var btn = document.getElementById("btn");
 var word = document.getElementById("startWord");
 var answer = document.getElementById("answer");
 var result = document.getElementById("result");
+var form = document.getElementById("wordForm");
 
 console.log(btn, word, answer);
 console.log(word.textContent[word.textContent.length - 1]);
-btn.addEventListener("click", function callbackfun() {
+form.addEventListener("submit", function callbackfun(eve) {
   var inputAnswer = answer.value;
+  eve.preventDefault();
   if (word.textContent[word.textContent.length - 1] === inputAnswer[0]) {
     console.log(inputAnswer);
     word.textContent = inputAnswer;
     answer.value = "";
+    answer.focus();
     result.textContent = `딩동댕~🔔`;
+  } else if (inputAnswer.length < 1) {
+    alert(`끝말잇기 게임이라구요..💢💥`);
+    answer.focus();
   } else {
     answer.value = "";
+    answer.focus();
     result.textContent = `❌땡❌`;
   }
 });
-
-/*
-var word = "시작";
-
-while (true) {
-  var answer = prompt(word);
-  if (word[word.length - 1] === answer[0]) {
-    word = answer;
-    alert(`딩동댕~🔔`);
-  } else {
-    alert(`❌땡❌`);
-  }
-}
-*/
